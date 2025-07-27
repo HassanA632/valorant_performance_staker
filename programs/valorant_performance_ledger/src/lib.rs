0c9a7f4a31943cc5816408b99bc2f221c5c86a07
HassanA632 <hassan.a6321@gmail.com>
2025-07-27 17:21:24 +0100

    feat: add deposit functionality + struct and error handling for unauthorized deposits

--- programs/valorant_performance_ledger/src/lib.rs
+++ programs/valorant_performance_ledger/src/lib.rs
@@ -2,6 +2,14 @@ use anchor_lang::prelude::*;
 
 declare_id!("DDbQzCa6kgnfZ667QFcBGbfUYGcjoPbnKbMNgTKedF2m");
 
+#[error_code]
+pub enum ErrorCode{
+    #[msg("Address provided does not have permission to deposit")]
+    UnauthorizedDepositAddress,
+    #[msg("Address has already deposited")]
+    AlreadyDeposited
+}
+
 #[program]
 pub mod valorant_performance_ledger {
     use super::*;
@@ -12,8 +20,41 @@ pub mod valorant_performance_ledger {
         sol_holder.authority = ctx.accounts.authority.key();
         sol_holder.allowed_depositors = allowed_depositors;
         sol_holder.deposits = [0; 5];
-        sol_holder.total_collected = 0;
+        sol_holder.total_collected = 0; 
         sol_holder.depositors_count = 0;
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64)-> Result<()>{
+        let sol_holder = &mut ctx.accounts.sol_holder;
+        let depositor = &ctx.accounts.depositor;
+
+        // Check if address executing deposit is whitelisted 
+        let depositor_index = sol_holder.allowed_depositors.iter()
+        .position(|&address| address == depositor.key())
+        .ok_or(ErrorCode::UnauthorizedDepositAddress)?;
+
+        // Check if user has already deposited
+        require!(sol_holder.deposits[depositor_index] == 0, ErrorCode::AlreadyDeposited);
+
+        //transfer SOL from depositer (user executing instruction) to contract
+        let transfer_instruction = anchor_lang::system_program::Transfer{
+            from: depositor.to_account_info(),
+            to: sol_holder.to_account_info(),
+        };
+        anchor_lang::system_program::transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                transfer_instruction,
+            ),
+            amount,
+        )?;
+
+        //update record
+        sol_holder.deposits[depositor_index] = amount;
+        sol_holder.total_collected += amount;
+        sol_holder.depositors_count +=1;
+        
         Ok(())
     }
 }
@@ -28,6 +69,15 @@ pub struct Initialize<'info> {
     pub sol_holder: Account<'info, SolHolder>,
     #[account(mut)]
     pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info>{
+    #[account(mut)]
+    pub sol_holder: Account<'info, SolHolder>,
+    #[account(mut)]
+    pub depositor: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
